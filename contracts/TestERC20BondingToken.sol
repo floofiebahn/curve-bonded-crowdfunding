f11636bcabd3a54ee8d0471d63c7f1548074bec4
Pavle Batuta <pavle.batuta@protonmail.com>
2019-04-12 18:03:33 +0200

    Initial changes

--- contracts/TestERC20BondingToken.sol
+++ contracts/TestERC20BondingToken.sol
@@ -1,11 +1,8 @@
-pragma solidity ^0.4.25;
+pragma solidity 0.5.6;
 
 import "./ERC20BondingToken.sol";
 
 contract TestERC20BondingToken is ERC20BondingToken {
-  function initialize(ERC20 _reserveToken, uint256 _initialPoolBalance, uint256 _initialSupply, uint32 _reserveRatio, uint256 _gasPrice) initializer public {
-    ERC20BondingToken.initialize(_reserveToken, _initialPoolBalance, _initialSupply, _reserveRatio, _gasPrice);
-  }
 
   function mint(uint256 amount) public {
     _curvedMint(amount);
