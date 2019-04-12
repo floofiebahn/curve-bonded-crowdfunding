910e0ad4e6d55cbb487b454e6848a6a3630c3d32
Pavle Batuta <pavle.batuta@protonmail.com>
2019-04-12 18:17:28 +0200

    Added deployment

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,7 @@
+const TestERC20BondingToken = artifacts.require("TestERC20BondingToken");
 const ERC20BondingToken = artifacts.require("ERC20BondingToken");
 
 module.exports = async (deployer, network, accounts) => {
-  await deployer.deploy(ERC20BondingToken);
+  await deployer.deploy(TestERC20BondingToken)
+  await deployer.deploy(ERC20BondingToken, TestERC20BondingToken.address);
 };
